3d42da14f98f65aeaf9db5a7900aae813ea885de
EvanJ <evanj3@illinois.edu>
2023-11-16 18:01:50 -0600

    Added frontend

--- Project/main.py
+++ Project/main.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-
-# Center-align the title
+# Title
 st.markdown("<h1 style='text-align: center;'>Stock Price Prediction Game</h1>", unsafe_allow_html=True)
 
 st.markdown("<h3 style='text-align: center;'>Choose your stocks:</h3>", unsafe_allow_html=True)
 
-# Add three dropdowns
+# Add stop options dropdowns
 stock1 = st.selectbox("Stock 1", ["Apple", "Tesla", "Nike"])
 stock2 = st.selectbox("Stock 2", ["Apple", "Tesla", "Nike"])
 stock3 = st.selectbox("Stock 3", ["Apple", "Tesla", "Nike"])
@@ -19,10 +18,7 @@ st.markdown("<h3 style='text-align: center;'>Choose your model to compete agains
 
 modelSelection = st.selectbox("Choose Model To Compete Against", ["GRU", "LSTM", "RNN"])
 
-# Add a submit button
+# submit button
 if st.button("Submit"):
     st.markdown("<h2 style='text-align: center;'>You Win!</h2>", unsafe_allow_html=True)  # Display "You Win" message
     st.markdown("<h2 style='text-align: center;'>Results:</h2>", unsafe_allow_html=True)  # Display "You Win" message
-
-# Add another dropdown
-
