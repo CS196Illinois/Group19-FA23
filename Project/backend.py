e630f61d3656d5cc9c42d3958558272cf486f5a0
nainamsingh <144743197+nainamsingh@users.noreply.github.com>
2023-11-29 17:43:51 -0600

    Added random stock choosing

--- Project/backend.py
+++ Project/backend.py
@@ -1,6 +1,7 @@
-import random
 import pandas as pd
+import random
 import yfinance as yf
+from datetime import datetime, timedelta
 import LSTM_model as LSTM
 import GRU_model as GRU
 import BidirectionalNN_model as BNN
@@ -11,6 +12,35 @@ from pymongo.server_api import ServerApi
 import functions as func
 import model as model
 import database_operations as db
+
+# Function to fetch historical stock data for a given company
+def fetch_stock_data(company_symbol, start_date, end_date):
+    stock_data = yf.download(company_symbol, start=start_date, end=end_date)
+    return stock_data
+
+def get_three_stocks(company_symbols):
+    # Number of random stocks to fetch
+    num_stocks_to_fetch = 3  # You can change this to any number you want
+
+    # Define the parameters for stock data fetching
+    start_date = datetime(2020, 1, 1)
+    end_date = datetime(2021, 12, 31)
+
+    # Randomly choose stock symbols to fetch data
+    random_stocks = random.sample(company_symbols, num_stocks_to_fetch)
+
+    return random_stocks
+
+# Define a list of stock symbols
+company_symbols = ['AAPL', 'GOOGL', 'MSFT', 'TSLA', 'AMZN', 'FB', 'GOOG', 'NVDA', 'AMD',
+                   'JPM', 'GS', 'WFC', 'C', 'BAC', 'XOM', 'CVX', 'BP', 'TOT',
+                   'INTC', 'IBM', 'ORCL', 'CSCO', 'QCOM', 'TXN', 'ADBE', 'CRM', 'NOW',
+                   'PG', 'KO', 'PEP', 'WMT', 'AMT', 'VZ', 'T', 'TMUS', 'SBUX',
+                   'JNJ', 'PFE', 'MRK', 'GSK', 'NVS', 'ABBV', 'LLY', 'BIIB', 'REGN']
+
+# Call the function
+selected_stocks = get_three_stocks(company_symbols)
+print(f"Selected Stocks: {selected_stocks}")
 
 # Load the pretrained LSTM model
 model = load_model("stock_prediction_model.py")
@@ -35,3 +65,7 @@ def game(random_stocks, number_of_days, model_type, player_stocks):
         return "You win!"
     else:
         return "Draw!"
+
+# Run the game with the randomly chosen stocks
+game_result = game(selected_stocks, 10, 'LSTM', selected_stocks)
+print(game_result)
